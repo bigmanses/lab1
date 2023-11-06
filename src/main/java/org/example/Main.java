da135c5ddf99aafb71ef945cf8fbb9e8326ae0b9
bigmanses <masur.ismailov05@gmail.com>
2023-11-06 15:06:23 +0300

    Рефакторинг

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ import windowinterface.InputFrame;
 
 public class Main {
     public static void main(String[] args) {
-        InputFrame frame = new InputFrame();
+        new InputFrame();
         }
     }
